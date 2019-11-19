4312e5ea414ea6bd73c020391f157a3933cc04ab
aluno <dforlani@gmail.com>
2019-11-18 22:25:38 -0300

    (1.3) Novo teste/feature: método equals, para testar se dois Dollar são iguais.

--- src/test/java/br/ifpr/money/TestDollar.java
+++ src/test/java/br/ifpr/money/TestDollar.java
@@ -1,6 +1,8 @@
 package br.ifpr.money;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -17,5 +19,10 @@ public class TestDollar {
         Dollar five = new Dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
+    }
+
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
